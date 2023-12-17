6adf4527fc2299ddf10f6de1246ee5210b58eb94
Albert-Rangel <albertsleyther@gmail.com>
2023-12-16 21:43:50 -0400

    small

--- test/repositories/cartRepositories.test.js
+++ test/repositories/cartRepositories.test.js
@@ -3,7 +3,7 @@ import cartRepositories from '../../src/repositories/cartRepositories.js';
 import Assert from 'assert'
 import chai, { expect } from 'chai'
 
-mongoose.connect('mongodb+srv://albertsleyther:09NbqGe9gecOLTBy@ecommerce.6lrddnh.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://albertto71:WwtuyUvrpVbqG622@cluster0.yk9l1lt.mongodb.net/?retryWrites=true&w=majority')
 
 const assert = Assert.strict
 
@@ -18,6 +18,13 @@ describe('Testeando el cartRepositories', () => {
         this.timeout(20000)
         const result = await this.cartRepositories.getcartsviaService()
         expect(result).to.be.an('array');
+    })
+
+    it('Deberia crear un carrito', async function () {
+        this.timeout(20000)
+        const result = await this.cartRepositories.addCartviaService()
+        const swBool = result.split("|")[0] === 'SUC' ? true : false
+         expect(swBool).to.be.true;
     })
 
     it('Deberia obtener un carrito por su cid', async function () {
--- test/repositories/productsRepositories.test.js
+++ test/repositories/productsRepositories.test.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 import productRepositories from '../../src/repositories/productsRepositories.js';
 import Assert from 'assert'
 import chai, { expect } from 'chai'
+import random from "random-string-generator";
 
-mongoose.connect('mongodb+srv://albertsleyther:09NbqGe9gecOLTBy@ecommerce.6lrddnh.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://albertto71:WwtuyUvrpVbqG622@cluster0.yk9l1lt.mongodb.net/?retryWrites=true&w=majority')
+
 
 const assert = Assert.strict
 
@@ -14,6 +16,28 @@ describe('Testeando el productRepositories', () => {
         this.productRepositorie = new productRepositories()
     })
 
+    it('Deberia agregar un nuevo producto', async function () {
+
+        this.timeout(20000)
+        const newProduct = {
+
+            title: "Test",
+            description: "test",
+            price: 1000,
+            thumbnail: "https://m.media-amazon.com/images/I/71Q0E2DYf2L.jpg",
+            code: random().toString(),
+            stock: 10,
+            status: true,
+            category: "Technology",
+            owner: 'adminCoder@coder.com'
+        }
+
+        const result = await this.productRepositorie.addProductviaService(newProduct)
+
+        const swBool = result.split("|")[0] === 'SUC' ? true : false
+        expect(swBool).to.be.true;
+    })
+
     it('Deberia retornar una lista de productos', async function () {
         this.timeout(20000)
         const result = await this.productRepositorie.getProductNpaginviaService()
@@ -22,7 +46,7 @@ describe('Testeando el productRepositories', () => {
 
     it('Deberia obtener un producto por su PID', async function () {
         this.timeout(20000)
-        var pid = '656f373d61740b17ec638d55'
+        var pid = '657e3697b4b73b1954ca209c'
         const result = await this.productRepositorie.getProductbyIDviaService(pid)
         expect(result).to.be.an('array');
     })
@@ -43,7 +67,7 @@ describe('Testeando el productRepositories', () => {
                 description: "test",
                 price: 1000,
                 thumbnail: "https://m.media-amazon.com/images/I/71Q0E2DYf2L.jpg",
-                code: "a22a25a250",
+                code: random().toString(),
                 stock: 10,
                 status: true,
                 category: "Technology",
